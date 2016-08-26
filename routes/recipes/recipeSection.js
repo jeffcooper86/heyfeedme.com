4023f0fb35b2d90603b3a1a5c79339e2920be5e6
Jeff Cooper <jeffcooperjc@gmail.com>
2016-08-26 13:50:51 -0400

    Added crumbs to recipe section.

--- routes/recipes/recipeSection.js
+++ routes/recipes/recipeSection.js
@@ -1,3 +1,13 @@
+var utils = require(process.cwd() + '/utils/global');
+
 exports = module.exports = function(req, res) {
-  res.render('recipes/recipeSection');
+  var l = res.locals,
+    template = 'recipes/recipeSection',
+    recipeSection = req.params.recipeSection;
+
+  l.crumbs = [
+    ['recipes', '/recipes'],
+    [utils.unslugify(recipeSection)]
+  ];
+  res.render(template);
 };
--- utils/global.js
+++ utils/global.js
@@ -5,6 +5,7 @@ module.exports.moment = _moment;
 module.exports.slugify = _slugify;
 module.exports.stripPrivates = _stripPrivates;
 module.exports.trimDirectories = _trimDirectories;
+module.exports.unslugify = _unslugify;
 
 function _moment(date) {
   return moment(date);
@@ -23,4 +24,8 @@ function _stripPrivates(schema) {
 function _trimDirectories(path, trimAmount) {
   if (path[-1] === '/') path = path.slice(0, -1);
   return path.split('/').slice(0, -trimAmount).join('/');
+}
+
+function _unslugify(route) {
+  return route.replace("-", " ");
 }
