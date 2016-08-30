c560e9e291013026820e324b9661d33e43ac0524
Jeff Cooper <jeffcooperjc@gmail.com>
2016-08-29 22:05:17 -0400

    Mocked up a recipe section page.

--- utils/recipes.js
+++ utils/recipes.js
@@ -1,8 +1,13 @@
 var _ = require('lodash');
 
 module.exports.getCategories = _getCategories;
+module.exports.getClassifications = _getClassifications;
 
 function _getCategories() {
   return 'breakfast:snacks:bread:pasta:sandwiches:main dishes:sides:salads:soups:dips:sauces:drinks:dessert'
     .split(':').sort();
+}
+
+function _getClassifications() {
+  return 'vegetarian:vegan:gluten free'.split(':').sort();
 }
