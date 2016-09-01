842f0fe55ff78ddab08f4c4b19fe3b5fdd41d1db
Jeff Cooper <jeffcooperjc@gmail.com>
2016-08-31 21:59:52 -0400

    Basic side navigation functionality.

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,5 +1,11 @@
 module.exports.doNav = _doNav;
 
 function _doNav() {
-  console.log('doing nav');
+  var $navControl = $('.js-nav-ctrl'),
+    $nav;
+
+  $navControl.on('click', function() {
+    $nav = $($(this).data('nav'));
+    $nav.toggleClass('active');
+  });
 }
