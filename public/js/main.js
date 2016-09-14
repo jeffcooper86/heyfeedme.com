cb7afafb675e3b69b9368040e65109a71b02d5dc
Jeff Cooper <jeffcooperjc@gmail.com>
2016-09-14 13:57:48 -0400

    Removing unused js.

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,13 @@
 var nav = require('./nav');
 var filters = require('./filters');
-var iUtils = require('../../utils/_shared/global');
 
 nav.doNav();
 filters.doFilters(function() {
-  var $recipes = $('.m-recipe-listings'),
-    $recipesTally = $('.js-recipes-tally');
+  var $recipes = $('.m-recipe-listings');
   $.ajax({
     url: '/api/recipes/html' + window.location.search
   }).done(function(data) {
     data = JSON.parse(data);
     $recipes.replaceWith(data.recipesHtml);
-    $recipesTally.html(iUtils.tally(data.recipesCount, ['recipe', 'recipes']));
   });
 });
