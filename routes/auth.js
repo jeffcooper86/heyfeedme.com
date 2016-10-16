17376db669e2c68e3b4c07e5aac54fdf60d6c46e
Jeff Cooper <jeffcooperjc@gmail.com>
2016-10-15 23:27:49 -0400

    Quick layout fix to auth.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 exports = module.exports = function(req, res, next) {
   if (req.method.toLowerCase() === 'post' &&
     req.body.password === process.env.ADMIN_ACCESS) {
-    req.session.auth = true
+    req.session.auth = true;
     return res.redirect(req.query.ref);
   }
   return res.render('auth');
