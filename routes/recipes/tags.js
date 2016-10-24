8111e0d5786ccd5589b54df1c3d3949e14def6aa
Jeff Cooper <jeffcooperjc@gmail.com>
2016-10-24 00:37:56 -0400

    Load recipe tags onto recipes tags view.

--- routes/recipes/tags.js
+++ routes/recipes/tags.js
@@ -1,4 +1,8 @@
-var recipesUtils = require(process.cwd() + '/utils/recipes');
+const async = require('async');
+
+const RecipeTags = require(process.cwd() + '/models/RecipeTag');
+const recipesUtils = require(process.cwd() + '/utils/recipes');
+const utils = require(process.cwd() + '/utils/global');
 
 exports = module.exports = function(req, res) {
   var l = res.locals,
@@ -13,5 +17,25 @@ exports = module.exports = function(req, res) {
   recipesUtils.setActiveCategories(res, activeSections);
   recipesUtils.setActiveClasses(res, activeClasses);
 
-  res.render(template);
+  async.waterfall([
+    getTags
+  ], function(err, tags) {
+    if (err) {
+      console.log(err);
+      return res.render('_error500');
+    }
+    l.data.tags = sortTags(tags);
+    res.render(template);
+  });
+
+  function getTags(cb) {
+    recipesUtils.getRecipesTags(RecipeTags, {}, cb);
+  }
+
+  function sortTags(tags) {
+    return utils.i.sortOn({
+      arr: tags,
+      val: 'name'
+    });
+  }
 };
